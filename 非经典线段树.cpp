a0e8ca5f80096fa41f1fdfe9911a349af1b73b9f
blues <1423923092@qq.com>
2025-10-03 16:40:18 +0800

    4

--- 非经典线段树.cpp
+++ 非经典线段树.cpp
@@ -121,7 +121,7 @@ int main(){
 }*/
 // 此外，线段树上也可以差分，线段树上差分本质上就是线段树上的范围修改操作
 //P1471 求方差
-#include<bits/stdc++.h>
+/*#include<bits/stdc++.h>
 using namespace std;
 
 const int maxn=100001;
@@ -208,6 +208,145 @@ int main(){
             int l,r;
             cin>>l>>r;
             printf("%.4lf\n",query(l,r,1,1,n,1)/(r-l+1)-query(l,r,0,1,n,1)/(r-l+1)*query(l,r,0,1,n,1)/(r-l+1));
+        }
+    }
+}*/
+
+//线段树上的序列操作，稍难一点
+//求连续1的最长子列，有点难度，容易打错
+#include<bits/stdc++.h>
+using namespace std;
+const int maxn=100001;
+int arr[maxn],sum[maxn<<2],pre0[maxn<<2],len0[maxn<<2],suf0[maxn<<2],pre1[maxn<<2],len1[maxn<<2],suf1[maxn<<2];
+int lazy[maxn<<2];
+bool update[maxn<<2];
+bool rever[maxn<<2];
+void up(int i,int ln,int rn){
+    int l=i<<1;
+    int r=i<<1|1;
+    sum[i]=sum[l]+sum[r];
+    len0[i]=max(max(len0[l],len0[r]),suf0[l]+pre0[r]);
+    pre0[i]=len0[l]<ln?pre0[l]:pre0[l]+pre0[r];
+    suf0[i]=len0[r]<rn?suf0[r]:suf0[l]+suf0[r];
+    len1[i]=max(max(len1[l],len1[r]),suf1[l]+pre1[r]);
+    pre1[i]=len1[l]<ln?pre1[l]:pre1[l]+pre1[r];
+    suf1[i]=len1[r]<rn?suf1[r]:suf1[l]+suf1[r];
+}
+void updateLazy(int i,int v,int n){
+    sum[i]=v*n;
+    pre0[i]=suf0[i]=len0[i]=v==0?n:0;
+    pre1[i]=suf1[i]=len1[i]=v==1?n:0;
+    lazy[i]=v;
+    update[i]=true;
+    rever[i]=false;
+}
+void reverseLazy(int i,int n){
+    sum[i]=n-sum[i];
+    swap(pre0[i],pre1[i]);
+    swap(len0[i],len1[i]);
+    swap(suf0[i],suf1[i]);
+    rever[i]=!rever[i];
+}
+void down(int i,int ln,int rn){
+    if(update[i]){
+        updateLazy(i<<1,lazy[i],ln);
+        updateLazy(i<<1|1,lazy[i],rn);
+        update[i]=false;
+    }
+    if(rever[i]){
+        reverseLazy(i<<1,ln);
+        reverseLazy(i<<1|1,rn);
+        rever[i]=false;
+    }
+}
+void build(int l,int r,int i){
+    if(l==r){
+        sum[i]=arr[l];
+        pre0[i]=arr[l]?0:1;
+        suf0[i]=arr[l]?0:1;
+        len0[i]=arr[l]?0:1;
+        pre1[i]=arr[l]?1:0;
+        suf1[i]=arr[l]?1:0;
+        len1[i]=arr[l]?1:0;
+        return;
+    }
+    int mid=(l+r)>>1;
+    build(l,mid,i<<1),build(mid+1,r,i<<1|1);
+    up(i,mid-l+1,r-mid);
+    rever[i]=0;
+    update[i]=0;
+}
+void Update(int jl,int jr,int v,int l,int r,int i){
+    if(jl<=l&&r<=jr){
+        updateLazy(i,v,r-l+1);
+    }else {
+        int mid=(l+r)>>1;
+        down(i,mid-l+1,r-mid);
+        if(jl<=mid) Update(jl,jr,v,l,mid,i<<1);
+        if(jr>mid) Update(jl,jr,v,mid+1,r,i<<1|1);
+        up(i,mid-l+1,r-mid);
+    }
+}
+void Reverse(int jl,int jr,int l,int r,int i){
+    if(jl<=l&&r<=jr){
+        reverseLazy(i,r-l+1);
+    }else {
+        int mid=(l+r)>>1;
+        down(i,mid-l+1,r-mid);
+        if(jl<=mid) Reverse(jl,jr,l,mid,i<<1);
+        if(jr>mid) Reverse(jl,jr,mid+1,r,i<<1|1);
+        up(i,mid-l+1,r-mid);
+    }
+}
+tuple<int,int,int> querylength(int jl,int jr,int l,int r,int i){
+    if(jl<=l&&r<=jr){
+        return make_tuple(len1[i],pre1[i],suf1[i]);
+    }
+    int mid=(l+r)>>1;
+    down(i,mid-l+1,r-mid);
+    if(jr<=mid) return querylength(jl,jr,l,mid,i<<1);
+    if(jl>mid) return querylength(jl,jr,mid+1,r,i<<1|1);
+    tuple<int,int,int> l1=querylength(jl,jr,l,mid,i<<1);
+    tuple<int,int,int> r1=querylength(jl,jr,mid+1,r,i<<1|1);
+    int llen=get<0>(l1),lpre=get<1>(l1),lsuf=get<2>(l1);
+    int rlen=get<0>(r1),rpre=get<1>(r1),rsuf=get<2>(r1);
+    int len=max(max(llen,rlen),lsuf+rpre);
+    int pre=lpre<mid-max(l,jl)+1?lpre:lpre+rpre;
+    int suf=rsuf<min(r,jr)-mid?rsuf:rsuf+lsuf;
+    return make_tuple(len,pre,suf);
+}
+int query(int jl,int jr,int l,int r,int i){
+    if(jl<=l&&r<=jr){
+        return sum[i];
+    }
+    int mid=(l+r)>>1;
+    down(i,mid-l+1,r-mid);
+    int ans=0;
+    if(jl<=mid) ans+=query(jl,jr,l,mid,i<<1); 
+    if(jr>mid)  ans+=query(jl,jr,mid+1,r,i<<1|1);
+    return ans;
+}
+int main(){
+    int n,m;
+    cin>>n>>m;
+    for(int i=1;i<=n;i++) cin>>arr[i];
+    build(1,n,1);
+    while(m--){
+        int opt,l,r;
+        cin>>opt>>l>>r;
+        l+=1;
+        r+=1;
+        if(opt==0){
+            Update(l,r,0,1,n,1);
+        }else if(opt==1){
+            Update(l,r,1,1,n,1);
+        }else if(opt==2){
+            Reverse(l,r,1,n,1);
+        }else if(opt==3){
+            cout<<query(l,r,1,n,1)<<endl;
+        }else {
+            tuple<int,int,int>a=querylength(l,r,1,n,1);
+            cout<<get<0>(a)<<endl;
         }
     }
 }
