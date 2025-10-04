93fd00b3143fca70177d52eab9a3afbddd01f2ee
blues <1423923092@qq.com>
2025-10-04 17:11:39 +0800

    4

--- 非经典线段树.cpp
+++ 非经典线段树.cpp
@@ -214,6 +214,7 @@ int main(){
 
 //线段树上的序列操作，稍难一点
 //求连续1的最长子列，有点难度，容易打错
+/*
 #include<bits/stdc++.h>
 using namespace std;
 const int maxn=100001;
@@ -347,6 +348,119 @@ int main(){
         }else {
             tuple<int,int,int>a=querylength(l,r,1,n,1);
             cout<<get<0>(a)<<endl;
+        }
+    }
+}
+*/
+
+//区间最值操作经典题 hdu 5306
+#include<bits/stdc++.h>
+using namespace std;
+const int maxn=1000001;
+const int minest=-1000001;
+long long arr[maxn<<2],sum[maxn<<2],mx[maxn<<2],sem[maxn<<2],cnt[maxn<<2];
+void up(int i){
+    int l=i<<1;
+    int r=i<<1|1;
+    sum[i]=sum[l]+sum[r];
+    mx[i]=max(mx[l],mx[r]);
+    if(mx[l]>mx[r]){
+        cnt[i]=cnt[l];
+        sem[i]=max(sem[l],mx[r]);
+    }else if(mx[r]>mx[l]){
+        cnt[i]=cnt[r];
+        sem[i]=max(sem[r],mx[l]);
+    }else {
+        sem[i]=max(sem[l],sem[r]);
+        cnt[i]=cnt[l]+cnt[r];
+    }
+}
+void Lazy(int i,int v){
+    if(v<mx[i]){
+        sum[i]-=(mx[i]-v)*cnt[i];
+        mx[i]=v;
+    }
+}
+void down(int i){
+    Lazy(i<<1,mx[i]);
+    Lazy(i<<1|1,mx[i]);
+}
+void setmin(int jl,int jr,int v,int l,int r,int i){
+    if(v>mx[i]) return;
+    if(jl<=l&&r<=jr&&v>sem[i]){
+        Lazy(i,v);
+        return;
+    }else {
+        down(i);
+        int mid=(l+r)>>1;
+        if(jl<=mid) setmin(jl,jr,v,l,mid,i<<1);
+        if(jr>mid) setmin(jl,jr,v,mid+1,r,i<<1|1);
+    }
+    up(i);
+}
+long long querymax(int jl,int jr,int l,int r,int i){
+    if(jl<=l&&r<=jr){
+        return mx[i];
+    }
+    down(i);
+    int mid=(l+r)>>1;
+    long long lm=minest,rm=minest;
+    if(jl<=mid) lm=querymax(jl,jr,l,mid,i<<1);
+    if(jr>mid)  rm=querymax(jl,jr,mid+1,r,i<<1|1);
+    return max(lm,rm);
+}
+long long querysum(int jl,int jr,int l,int r,int i){
+    if(jl<=l&&r<=jr){
+        return sum[i];
+    }
+    down(i);
+    int mid=(l+r)>>1;
+    long long ans=0;
+    if(jl<=mid) ans+=querysum(jl,jr,l,mid,i<<1);
+    if(jr>mid)  ans+=querysum(jl,jr,mid+1,r,i<<1|1);
+    return ans;
+}
+void build(int l,int r,int i){
+    if(l==r){
+        sum[i]=arr[l];
+        mx[i]=arr[l];
+        sem[i]=minest;
+        cnt[i]=1;
+        return;
+    }
+    int mid=(l+r)>>1;
+    build(l,mid,i<<1),build(mid+1,r,i<<1|1);
+    up(i);
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--){
+        int n,m;
+        cin>>n>>m;
+        for(int i=1;i<=n;i++){
+            cin>>arr[i];
+        }
+        build(1,n,1);
+        while(m--){
+            int opt;
+            cin>>opt;
+            if(opt==0){
+                int l,r;
+                long long mi;
+                cin>>l>>r>>mi;
+                setmin(l,r,mi,1,n,1);
+            }else if(opt==1){
+                int l,r;
+                cin>>l>>r;
+                cout<<querymax(l,r,1,n,1)<<endl;
+            }else {
+                int l,r;
+                cin>>l>>r;
+                cout<<querysum(l,r,1,n,1)<<endl;
+            }
         }
     }
 }
